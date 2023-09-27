65192a23d43cede9302446c3188638d53ce2540e
Jorge Contreras <86329049+jorgeostos@users.noreply.github.com>
2023-09-27 17:48:14 -0600

    set up color mode options on theme

--- theme/theme.js
+++ theme/theme.js
@@ -1,9 +1,17 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark', // Choose the initial color mode
+    useSystemColorMode: false // Use the system color mode if true, or let users choose manually
+  },
   fonts: {
     body: "'Inter', sans-serif",
     heading: "'Inter', sans-serif"
+  },
+  colors: {
+    light: {},
+    dark: {}
   }
 })
 export default theme
